1575b1f6045dece4df64b02e90c122ee65cd40ce
Hao Guo <jennnnyguo@gmail.com>
2023-05-16 16:27:53 +1000

    Done with assignmeny

--- index.js
+++ index.js
@@ -1,35 +1,30 @@
-const dodger = document.getElementById("dodger");
-//=> 抓住躲闪器
+const dodger = document.getElementById("dodger")
 
 function moveDodgerLeft() {
-    const leftNumber = dodger.style.left.replace("px", "");
-    const left = parseInt(leftNumber, 10);
+    const leftNumbers = dodger.style.left.replace("px", "")
+    const left = parseInt(leftNumbers, 10)
 
     if (left > 0) {
-        dodger.style.left = `${left - 1}px`;
+        dodger.style.left = `${left - 1}px`
     }
 }
-//=> 构建我们的moveDodgerLeft()函数，添加对躲避者当前位置的检查
-//=> 确保 dodger 的左边缘没有到达其容器的左边缘。
 
 document.addEventListener("keydown", function (e) {
     if (e.key === "ArrowLeft") {
-        moveDodgerLeft();
+        moveDodgerLeft()
     }
-});
-//=> 连接到我们的事件监听器：
+})
 
 function moveDodgerRight() {
-    const leftNumber = dodger.style.left.replace("px", "");
-    const left = parseInt(leftNumber, 10);
-
+    const leftNumber = dodger.style.left.replace("px", "")
+    const left = parseInt(leftNumber, 10)
     if (left < 360) {
-        dodger.style.left = `${left + 1}px`;
+        dodger.style.left = `${left + 1}px`
     }
 }
 
 document.addEventListener("keydown", function (e) {
     if (e.key === "ArrowRight") {
-        moveDodgerRight();
+        moveDodgerRight()
     }
-});
+})
